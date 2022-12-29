2c531ddc9577bd3ecae85c61cbc68b44de5df196
Danseren <104249031+Danseren@users.noreply.github.com>
2022-12-29 17:46:14 +0500

    Initial commit

---
+++ app/src/main/java/ru/sample/store/binscaner/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.sample.store.binscaner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.sample.store.binscaner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.containerMain, MainFragment.newInstance())
+                .commit()
+        }
+    }
+}
---
+++ app/src/main/java/ru/sample/store/binscaner/MainFragment.kt
@@ -0,0 +1,46 @@
+package ru.sample.store.binscaner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import ru.sample.store.binscaner.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textField.setEndIconOnClickListener {
+            val activity = context as AppCompatActivity
+            activity.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.containerMain, BinInfoFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
